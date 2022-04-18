c41896f6cc70dfe6b0b65c36cf7a5362749f9696
Jacek Grzegrzolka <jacekgrzegrzolka@gmail.com>
2022-04-18 21:39:27 +0200

    finished project

--- interface.py
+++ interface.py
@@ -1,7 +1,21 @@
-from operator import is_
-import tkinter
-import time
-from datetime import timedelta
+try:
+    from logging import exception
+    from operator import is_
+    import tkinter
+    import time
+    from datetime import timedelta
+
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    import pandas as pd
+except:
+     print("can't load all packages")
+
+is_paused = True
+started = 0
+ended = 0
+elapsed = 0
+
 
 def start_clicked():
     global started
@@ -17,15 +31,58 @@ def start_clicked():
 
 def stop_clicked():
     global log
+    global ended
+    global elapsed
     global is_paused
     if not is_paused:
         is_paused = True
+        ended = time.time()
         elapsed = time.time() - started
-        output_info = "elapsed: " + str(timedelta(seconds=elapsed))
+        output_info = "elapsed: " + str(timedelta(seconds=round(elapsed,0)))
         log.set(output_info)
+        try:
+            write_to_drive()
+            # time.sleep(3)
+            # quit()
+        except:
+            print('last error')
+            quit()
 
-is_paused = True
-started = 0
+def sync():
+    is_paused = True
+
+def write_to_drive():
+    global started
+    global ended
+    global elapsed
+    
+    try:
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+        drive = GoogleDrive(gauth)
+    except:
+            print("authentification error")
+    try:
+        fileDownloaded = drive.CreateFile({'id':'1zTuFXiai6eyDbejX1tyKt9B6phYUEj_C'})
+        fileDownloaded.GetContentFile('data.csv')
+    except:
+        print("can't open the file")
+
+    try:
+        myCsvRow = str(time.strftime("%m-%d-%Y %H:%M:%S", time.localtime(int(started)))) + ';' + str(time.strftime("%m-%d-%Y %H:%M:%S", time.localtime(int(ended)))) + ';'+ str(timedelta(seconds=round(elapsed,0))) + '\n'
+        with open('data.csv','a') as fd:
+            fd.write(myCsvRow)
+        fileDownloaded.SetContentFile(f'data.csv')
+        fileDownloaded.Upload()
+        print('file uploaded successfully')
+    except:
+        print('error opening the file')
+        quit()
+
+
+
+
+
 
 app = tkinter.Tk()
 app.geometry('180x240')
